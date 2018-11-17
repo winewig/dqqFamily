d4f0c081a570dba2b48d0b783b4dc171b5e8c8a4
qinyuan.zhang@elektrobit.com <qinyuan.zhang@elektrobit.com>
2018-11-17 13:54:33 +0100

    feature: finished the main page animation

--- src/app/main-page-area/main-page/main-page.component.ts
+++ src/app/main-page-area/main-page/main-page.component.ts
@@ -7,34 +7,56 @@ import {animate, sequence, state, style, transition, trigger} from '@angular/ani
   styleUrls: ['./main-page.component.scss'],
   animations: [
     trigger('papaHead', [
-      // state('toCenter', style({
-      //   transform: 'translate( 50vw, 80px )',
-      // })),
       transition(
         ':enter',
         sequence([
           style({
-            transform: 'translate(-80px, 80px)'
+            position: 'absolute',
+            transform: 'translateX(-50vw)',
+            opacity: 0
           }),
           animate('2s')
         ])
       )
     ]),
     trigger('mamaHead', [
-      state('toCenter', style({
-        transform: 'translate( 50vw, 160px )'
-      })),
       transition(
         ':enter',
         sequence(([
           style({
-            transform: 'translate(100vw, 160px)'
+            position: 'absolute',
+            transform: 'translateX(50vw)',
+            opacity: 0
           }),
           animate('2s')
         ]))
       )
     ]),
-
+    trigger('imgBottom', [
+      transition(
+        ':enter',
+        sequence([
+          style({
+            transform: 'translateY(50vh)',
+            opacity: 0
+          }),
+          animate('2s 2s'),
+          style({
+            opacity: 1,
+            transform: 'rotateZ(50deg)'
+          }),
+          animate('0.5s'),
+          style({
+            transform: 'rotateZ(-50deg)'
+          }),
+          animate('0.5s'),
+          style({
+            transform: 'scale(2,2'
+          }),
+          animate('1s')
+        ])
+      )
+    ])
   ]
 })
 export class MainPageComponent implements OnInit {
