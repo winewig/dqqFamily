3d592c778b9e9d25c19197f5d44aa9e8ad4e16c3
el_qizh0003 <qinyuan.zhang@elektrobit.com>
2018-11-20 16:14:16 +0100

    added routing debug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,16 @@ import {MainPageComponent} from './main-page-area/main-page/main-page.component'
 import {ThirteenMonthComponent} from './main/thirteen-month/thirteen-month.component';
 
 const appRoutes: Routes = [
+  {path: '13month', component: ThirteenMonthComponent},
   {path: '', component: MainPageComponent},
-  {path: '13month', component: ThirteenMonthComponent}
+  // Todo: page not found: { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   exports: [RouterModule],
   declarations: []
